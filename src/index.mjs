88346f9162786c525b742c0234eb7192489999a9
custompro98 <mitchjoa@gmail.com>
2024-08-29 15:54:13 -0400

    remove console log

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,1 @@
 export { rollOnTable } from "./tables/index.mjs";
-
-import { rollOnTable } from "./tables/index.mjs";
-for (let i = 0; i < 1_000_000; i++) {
-  console.log(rollOnTable("spellbooks"));
-}
